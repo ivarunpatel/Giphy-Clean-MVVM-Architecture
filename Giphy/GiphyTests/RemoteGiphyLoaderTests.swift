a97b887735137c7828a2883191a9e2a699a40401
Varun Patel <varun@thepeoplemesh.com>
2023-04-11 12:21:50 +0530

    Guarantee that we can `load` more than once

--- Giphy/GiphyTests/RemoteGiphyLoaderTests.swift
+++ Giphy/GiphyTests/RemoteGiphyLoaderTests.swift
@@ -24,6 +24,16 @@ final class RemoteGiphyLoaderTests: XCTestCase {
         XCTAssertNotNil(client.requestedURL)
     }
     
+    func test_loadTwice_requestsDataFromURLTwice() {
+        let url = URL(string: "http://any-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+    
     // MARK: - Helper
     
     private func makeSUT(url: URL = URL(string: "http://any-url.com")!) -> (RemoteGiphyLoader, HTTPClientSpy) {
@@ -34,11 +44,12 @@ final class RemoteGiphyLoaderTests: XCTestCase {
     
     final class HTTPClientSpy: HTTPClient {
         var requestedURL: URL?
+        var requestedURLs = [URL]()
         
         func get(from url: URL) {
             requestedURL = url
+            requestedURLs.append(url)
         }
-        
     }
 
 }
