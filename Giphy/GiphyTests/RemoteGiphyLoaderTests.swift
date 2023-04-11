dee60b226cde238c23bfee1b43edb10854bca925
Varun Patel <varun@thepeoplemesh.com>
2023-04-11 12:44:16 +0530

    Combine spy captured params into a single type (tuple)

--- Giphy/GiphyTests/RemoteGiphyLoaderTests.swift
+++ Giphy/GiphyTests/RemoteGiphyLoaderTests.swift
@@ -56,16 +56,17 @@ final class RemoteGiphyLoaderTests: XCTestCase {
     }
     
     final class HTTPClientSpy: HTTPClient {
-        var requestedURLs = [URL]()
-        var completions = [(Error) -> Void]()
+        var messages = [(url: URL, completion: (Error) -> Void)]()
+        var requestedURLs: [URL] {
+            messages.map { $0.url }
+        }
         
         func get(from url: URL, completion: @escaping (Error) -> Void) {
-            requestedURLs.append(url)
-            completions.append(completion)
+            messages.append((url, completion))
         }
         
         func complete(with error: Error, at index: Int = 0) {
-            completions[index](error)
+            messages[index].completion(error)
         }
     }
 
