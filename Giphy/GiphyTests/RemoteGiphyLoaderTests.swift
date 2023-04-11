681ceb32d1a9b72e82689fa6df82a266eeff1efc
Varun Patel <varun@thepeoplemesh.com>
2023-04-11 11:37:05 +0530

    Request data from URL on `load` call

--- Giphy/GiphyTests/RemoteGiphyLoaderTests.swift
+++ Giphy/GiphyTests/RemoteGiphyLoaderTests.swift
@@ -8,19 +8,41 @@
 import XCTest
 
 class RemoteGiphyLoader {
+    let client: HTTPClient
     
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func load() {
+        client.requestedURL = URL(string: "http://any-url.com")!
+    }
 }
 
-class HTTPClient {
-    var requestedURL: String?
+final class HTTPClient {
+    
+    init() {
+        
+    }
+    
+    var requestedURL: URL?
 }
 
 final class RemoteGiphyLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
         let client = HTTPClient()
-        _ = RemoteGiphyLoader()
+        _ = RemoteGiphyLoader(client: client)
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPClient()
+        let sut = RemoteGiphyLoader(client: client)
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 }
