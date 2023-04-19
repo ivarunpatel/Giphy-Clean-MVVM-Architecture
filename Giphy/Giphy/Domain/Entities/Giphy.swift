66ce7ac1c7c3f2f66b30032d5778d3689111484e
Varun Patel <varun@thepeoplemesh.com>
2023-04-19 16:35:38 +0530

    `TrendingGiphyUseCase` `execute` successfully load giphy on success

--- Giphy/Giphy/Domain/Entities/Giphy.swift
+++ Giphy/Giphy/Domain/Entities/Giphy.swift
@@ -12,17 +12,35 @@ public struct Giphy: Equatable {
     let title: String
     let datetime: String
     let images: GiphyImages
+    
+    public init(id: String, title: String, datetime: String, images: GiphyImages) {
+        self.id = id
+        self.title = title
+        self.datetime = datetime
+        self.images = images
+    }
 }
 
 public struct GiphyImages: Equatable {
     let original: GiphyImageMetadata
     let small: GiphyImageMetadata
+    
+    public init(original: GiphyImageMetadata, small: GiphyImageMetadata) {
+        self.original = original
+        self.small = small
+    }
 }
 
 public struct GiphyImageMetadata: Equatable {
     let height: String
     let width: String
     let url: URL
+    
+    public init(height: String, width: String, url: URL) {
+        self.height = height
+        self.width = width
+        self.url = url
+    }
 }
 
 public struct GiphyPage: Equatable {
@@ -30,4 +48,11 @@ public struct GiphyPage: Equatable {
     let count: Int
     let offset: Int
     let giphy: [Giphy]
+    
+    public init(totalCount: Int, count: Int, offset: Int, giphy: [Giphy]) {
+        self.totalCount = totalCount
+        self.count = count
+        self.offset = offset
+        self.giphy = giphy
+    }
 }
---
+++ Giphy/GiphyTests/Data/TrendingGiphyUseCaseTests.swift
@@ -0,0 +1,91 @@
+//
+//  TrendingGiphyUseCaseTests.swift
+//  GiphyTests
+//
+//  Created by Varun on 19/04/23.
+//
+
+import XCTest
+import Giphy
+
+final class TrendingGiphyUseCase {
+    
+    let trendingGiphyRepository: TrendingGiphyRepository
+    
+    init(trendingGiphyRepository: TrendingGiphyRepository) {
+        self.trendingGiphyRepository = trendingGiphyRepository
+    }
+    
+    func execute(requestValue: TrendingGiphyUseCaseRequestValue, completion: @escaping (Result<GiphyPage, Error>) -> Void) -> Cancellable? {
+        return trendingGiphyRepository.fetchTrendingGiphyList(limit: requestValue.limit) { result in
+            switch result {
+            case .success(let giphyPage):
+                completion(.success(giphyPage))
+            default: break
+            }
+        }
+    }
+}
+
+struct TrendingGiphyUseCaseRequestValue {
+    let limit: Int
+}
+
+final class TrendingGiphyUseCaseTests: XCTestCase {
+    
+    func test_execute_deliversTrendingGiphy() {
+        let (sut, repository) = makeSUT()
+        
+        let expectedResult = GiphyPage(totalCount: 20, count: 10, offset: 0, giphy: [Giphy(id: "1", title: "title", datetime: "any time", images: GiphyImages(original: GiphyImageMetadata(height: "500", width: "500", url: anyURL()), small: GiphyImageMetadata(height: "100", width: "100", url: anyURL())))])
+        
+        let requestValue = TrendingGiphyUseCaseRequestValue(limit: 10)
+        expect(sut: sut, requestValue: requestValue, toCompleteWith: .success(expectedResult)) {
+            repository.complete(with: expectedResult)
+        }
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (useCase: TrendingGiphyUseCase, repository: TrendingGiphyRepositorySpy) {
+        let repository = TrendingGiphyRepositorySpy()
+        let useCase = TrendingGiphyUseCase(trendingGiphyRepository: repository)
+        trackForMemoryLeaks(repository, file: file, line: line)
+        trackForMemoryLeaks(useCase, file: file, line: line)
+        return (useCase, repository)
+    }
+    
+    private func expect(sut: TrendingGiphyUseCase, requestValue: TrendingGiphyUseCaseRequestValue, toCompleteWith expectedResult: Result<GiphyPage, Error>, action: () -> Void, file: StaticString = #file, line: UInt = #line) {
+        
+        let expectation = expectation(description: "Waiting for completion")
+        _ = sut.execute(requestValue: requestValue) { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case (.success(let receivedGiphyPage), .success(let expectedGiphyImage)):
+                XCTAssertEqual(receivedGiphyPage, expectedGiphyImage, file: file, line: line)
+            case (.failure(let receivedError as NSError), .failure(let expectedError as NSError)):
+                XCTAssertEqual(receivedError.domain, expectedError.domain, file: file, line: line)
+                XCTAssertEqual(receivedError.code, expectedError.code, file: file, line: line)
+            default:
+                XCTFail("Expected to receive \(expectedResult), got \(receivedResult) instead", file: file, line: line)
+            }
+            expectation.fulfill()
+        }
+        
+        action()
+        
+        wait(for: [expectation], timeout: 1.0)
+    }
+    
+    final class TrendingGiphyRepositorySpy: TrendingGiphyRepository {
+        private var receivedMessages = [(TrendingGiphyRepository.Result) -> Void]()
+        
+        func fetchTrendingGiphyList(limit: Int, completion: @escaping (TrendingGiphyRepository.Result) -> Void) -> Cancellable? {
+            receivedMessages.append(completion)
+            return nil
+        }
+        
+        func complete(with giphyPage: GiphyPage, at index: Int = 0) {
+            receivedMessages[index](.success(giphyPage))
+        }
+    }
+
+}
