a9709c9040674ca0e8da73c0df6aed45dae6b33e
Varun Patel <varun@thepeoplemesh.com>
2023-04-27 13:08:19 +0530

    `FeedViewModel` `viewDidLoad` load items from use case

--- Giphy/GiphyTests/Helpers/SharedTestHelpers.swift
+++ Giphy/GiphyTests/Helpers/SharedTestHelpers.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Giphy
 
 func anyURL() -> URL {
     URL(string: "http://any-url.com")!
@@ -17,4 +18,8 @@ func anyNSError() -> NSError {
 
 func anyData() -> Data {
     Data("any data".utf8)
+}
+
+func makeFeedItem() -> FeedPage {
+    FeedPage(totalCount: 20, count: 10, offset: 0, giphy: [Feed(id: "1", title: "title", datetime: "any time", images: FeedImages(original: FeedImageMetadata(height: "500", width: "500", url: anyURL()), small: FeedImageMetadata(height: "100", width: "100", url: anyURL())), user: FeedUser(username: "test", displayName: "test_name"))])
 }
--- Giphy/GiphyTests/Presentation/Feed/FeedViewModel/FeedViewModelTests.swift
+++ Giphy/GiphyTests/Presentation/Feed/FeedViewModel/FeedViewModelTests.swift
@@ -15,6 +15,15 @@ final class FeedViewModel {
     init(useCase: TrendingUseCase) {
         self.useCase = useCase
     }
+    
+    var items: Observable<FeedPage?> = Observable(.none)
+    
+    func viewDidLoad() {
+        _ = useCase.execute(requestValue: .init(limit: 10)) { [weak self] result in
+            guard let self = self else { return }
+            items.value = try! result.get()
+        }
+    }
 }
 
 final class FeedViewModelTests: XCTestCase {
@@ -26,14 +35,38 @@ final class FeedViewModelTests: XCTestCase {
         XCTAssertTrue(useCase.receivedMessages.isEmpty)
     }
     
+    func test_viewDidLoad_loadItems() {
+        let (sut, useCase) = makeSUT()
+        
+        sut.viewDidLoad()
+        
+        let expectedValue = makeFeedItem()
+        useCase.complete(with: expectedValue)
+        
+        XCTAssertEqual(sut.items.value, expectedValue)
+    }
+    
     // MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedViewModel, useCase: TrendingUseCaseSpy) {
+        let useCase = TrendingUseCaseSpy()
+        let sut = FeedViewModel(useCase: useCase)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(useCase, file: file, line: line)
+        return (sut, useCase)
+    }
     
     private final class TrendingUseCaseSpy: TrendingUseCase {
         public private(set) var receivedMessages = [(Result<FeedPage, Error>) -> Void]()
         
         func execute(requestValue: TrendingGiphyUseCaseRequestValue, completion: @escaping (Result<FeedPage, Error>) -> Void) -> Cancellable? {
+            receivedMessages.append(completion)
             return nil
         }
+        
+        func complete(with feed: FeedPage, at index: Int = 0) {
+            receivedMessages[index](.success(feed))
+        }
     }
-
+    
 }
