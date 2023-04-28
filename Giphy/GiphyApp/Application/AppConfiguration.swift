28edd118b8cfb1f223bcf4f7404cf6fc4861cb91
Varun Patel <varun@thepeoplemesh.com>
2023-04-28 17:27:47 +0530

    Add `AppConfiguration` and setup base url and api key

---
+++ Giphy/GiphyApp/Application/AppConfiguration.swift
@@ -0,0 +1,25 @@
+//
+//  AppConfiguration.swift
+//  GiphyApp
+//
+//  Created by Varun on 28/04/23.
+//
+
+import Foundation
+
+final class AppConfiguration {
+    
+    lazy var apiKey: String = {
+        guard let apiKey = Bundle(for: AppConfiguration.self).object(forInfoDictionaryKey: "ApiKey") as? String else {
+            fatalError("ApiKey cannot be empty in info.plist")
+        }
+        return apiKey
+    }()
+    
+    lazy var baseURL: String = {
+        guard let baseURL = Bundle(for: AppConfiguration.self).object(forInfoDictionaryKey: "BaseURL") as? String else {
+            fatalError("BaseURL cannot be empty in info.plist")
+        }
+        return baseURL
+    }()
+}
