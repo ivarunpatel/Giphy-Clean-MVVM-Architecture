c780cf95a1cdaae83e85d937154d5c66b3241302
Varun Patel <varun@thepeoplemesh.com>
2023-04-28 17:02:40 +0530

    Create `GiphyApp` iOS target

---
+++ Giphy/GiphyApp/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  GiphyApp
+//
+//  Created by Varun on 28/04/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Giphy/GiphyApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GiphyApp
+//
+//  Created by Varun on 28/04/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
